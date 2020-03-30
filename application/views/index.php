f1f27b031b07188f284a25cc2cd06cfaf7e8658b
yogizeyq <yogi.ode@gmail.com>
2020-03-30 20:02:29 +0800

    penambahan template website

---
+++ application/views/index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Techno Creative</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/display/img/favicon.png">
+
+     <link href="<?php echo base_url("assets/display/font-awesome/css/font-awesome.min.css"); ?>" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/bootstrap.min.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/owl.carousel.min.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/magnific-popup.css") ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/themify-icons.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/nice-select.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/flaticon.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/gijgo.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/animate.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/slicknav.css") ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/display/css/style.css") ?>">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+
+        <header>
+            <?php include'menu.php'; ?>
+        </header>
+        <!-- header-end -->
+
+      <?php echo $konten; ?>
+
+
+        <!-- footer -->
+        <footer class="footer footer_bg_1">
+            <div class="footer_top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-md-6 col-lg-4">
+                            <div class="footer_widget">
+                                <div class="footer_logo">
+                                    <a href="#">
+                                        <img src="assets/display/img/logo.png" alt="">
+                                    </a>
+                                </div>
+                                <p>
+                                    Firmament morning sixth subdue darkness creeping gathered divide our let god moving.
+                                    Moving in fourth air night bring upon it beast let you dominion likeness open place day
+                                    great.
+                                </p>
+                                <div class="socail_links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <i class="ti-facebook"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="ti-twitter-alt"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa fa-instagram"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i class="fa fa-youtube-play"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
+                            <div class="footer_widget">
+                                <h3 class="footer_title">
+                                    Courses
+                                </h3>
+                                <ul>
+                                    <li><a href="#">Wordpress</a></li>
+                                    <li><a href="#"> Photoshop</a></li>
+                                    <li><a href="#">Illustrator</a></li>
+                                    <li><a href="#">Adobe XD</a></li>
+                                    <li><a href="#">UI/UX</a></li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-md-6 col-lg-2">
+                            <div class="footer_widget">
+                                <h3 class="footer_title">
+                                    Resourches
+                                </h3>
+                                <ul>
+                                    <li><a href="#">Free Adobe XD</a></li>
+                                    <li><a href="#">Tutorials</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#"> About</a></li>
+                                    <li><a href="#"> Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 col-lg-3">
+                            <div class="footer_widget">
+                                <h3 class="footer_title">
+                                    Address
+                                </h3>
+                                <p>
+                                    200, D-block, Green lane USA <br>
+                                    +10 367 467 8934 <br>
+                                    edumark@contact.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copy-right_text">
+                <div class="container">
+                    <div class="footer_border"></div>
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <p class="copy_right text-center">
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer -->
+
+
+        <!-- form itself end-->
+        <form id="test-form" class="white-popup-block mfp-hide">
+            <div class="popup_box ">
+                <div class="popup_inner">
+                    <div class="logo text-center">
+                        <a href="#">
+                            <img src="assets/display/img/form-logo.png" alt="">
+                        </a>
+                    </div>
+                    <h3>Sign in</h3>
+                    <form action="#">
+                        <div class="row">
+                            <div class="col-xl-12 col-md-12">
+                                <input type="email" placeholder="Enter email">
+                            </div>
+                            <div class="col-xl-12 col-md-12">
+                                <input type="password" placeholder="Password">
+                            </div>
+                            <div class="col-xl-12">
+                                <button type="submit" class="boxed_btn_orange">Sign in</button>
+                            </div>
+                        </div>
+                    </form>
+                    <p class="doen_have_acc">Don’t have an account? <a class="dont-hav-acc" href="#test-form2">Sign Up</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+        <!-- form itself end -->
+
+        <!-- form itself end-->
+        <form id="test-form2" class="white-popup-block mfp-hide">
+            <div class="popup_box ">
+                <div class="popup_inner">
+                    <div class="logo text-center">
+                        <a href="#">
+                            <img src="assets/display/img/form-logo.png" alt="">
+                        </a>
+                    </div>
+                    <h3>Resistration</h3>
+                    <form action="#">
+                        <div class="row">
+                            <div class="col-xl-12 col-md-12">
+                                <input type="email" placeholder="Enter email">
+                            </div>
+                            <div class="col-xl-12 col-md-12">
+                                <input type="password" placeholder="Password">
+                            </div>
+                            <div class="col-xl-12 col-md-12">
+                                <input type="Password" placeholder="Confirm password">
+                            </div>
+                            <div class="col-xl-12">
+                                <button type="submit" class="boxed_btn_orange">Sign Up</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </form>
+        <!-- form itself end -->
+
+
+        <!-- JS here -->
+        <script src="<?php echo base_url("assets/display/js/vendor/modernizr-3.5.0.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/vendor/jquery-1.12.4.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/popper.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/bootstrap.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/owl.carousel.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/isotope.pkgd.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/ajax-form.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/waypoints.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.counterup.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/imagesloaded.pkgd.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/scrollIt.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.scrollUp.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/wow.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/nice-select.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.slicknav.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.magnific-popup.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/plugins.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/gijgo.min.js") ?>"></script>
+
+        <!--contact js-->
+        <script src="<?php echo base_url("assets/display/js/contact.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.ajaxchimp.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.form.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/jquery.validate.min.js") ?>"></script>
+        <script src="<?php echo base_url("assets/display/js/mail-script.js") ?>"></script>
+
+        <script src="<?php echo base_url("assets/display/js/main.js") ?>"></script>
+    </body>
+
+    </html>
